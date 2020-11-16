0977dfc0b564585cc1a100084aaf9f635ac86340
pandyah5 <pandyahetav1@gmail.com>
2020-11-16 02:19:53 -0500

    Edited shift_reg, added main

--- shift_reg_parallel_load.v
+++ shift_reg_parallel_load.v
@@ -1,7 +1,47 @@
+`timescale 1ns / 1ps
+`default_nettype none
+
+// DE1_SoC board needs the below main module
+
+module main	(
+	input wire CLOCK_50,            //On Board 50 MHz
+	input wire [9:0] SW,            // On board Switches
+	input wire [3:0] KEY,           // On board push buttons
+	output wire [6:0] HEX0,         // HEX displays
+	output wire [6:0] HEX1,
+	output wire [6:0] HEX2,
+	output wire [6:0] HEX3,
+	output wire [6:0] HEX4,
+	output wire [6:0] HEX5,
+	output wire [9:0] LEDR,         // LEDs
+	output wire [7:0] x,            // VGA pixel coordinates
+	output wire [6:0] y,
+	output wire [2:0] colour,       // VGA pixel colour (0-7)
+	output wire plot,               // Pixel drawn when this is pulsed
+	output wire vga_resetn      	// VGA resets to black when this is pulsed (NOT CURRENTLY AVAILABLE)
+);
+	top v1(SW, LEDR);
+
+endmodule
+
+// Top Module
+
+module top(SW, LEDR);
+	input [9:0] SW;
+	output [9:0] LEDR;
+
+	shift_reg_parallel_load u1 (SW[4], SW[3:0], SW[9], SW[8], SW[7], LEDR[3:0]);
+//  shift_reg_parallel_load_1 u1 (SW[4], SW[3:0], SW[9], SW[8], SW[7], LEDR[3:0]);
+//  You can try the above too - just uncomment the one you wanna try
+
+endmodule
+
+// Register - n bit
+
 module shift_reg_parallel_load(w, d, clock, loadn, resetp, q); // Synchronous and Active High
-    input w, clock, loadn, resetn;
+    input w, clock, loadn, resetp;
     input[3:0] d;
-    output[3:0] q;
+    output reg [3:0] q;
 
     always@(posedge clock)
         begin
@@ -19,10 +59,10 @@ module shift_reg_parallel_load(w, d, clock, loadn, resetp, q); // Synchronous an
         end
 endmodule
 
-module shift_reg_parallel_load(w, d, clock, loadn, resetn, q); // Asynchronous and Active low
+module shift_reg_parallel_load_1(w, d, clock, loadn, resetn, q); // Asynchronous and Active low
     input w, clock, loadn, resetn;
     input[3:0] d;
-    output[3:0] q;
+    output reg [3:0] q;
 
     always@(posedge clock, negedge resetn)
         begin
